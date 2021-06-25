f19cd44a8238b5151a281e4c8efa6aacd8b92b44
txazo <784990655@qq.com>
2021-06-25 16:47:45 +0800

    auto commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # java-samples
+
 Java samples
---
+++ java-samples-sdk/src/main/java/org/txazo/java/samples/sdk/SdkAutoConfiguration.java
@@ -0,0 +1,11 @@
+package org.txazo.java.samples.sdk;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author tuxiaozhou
+ * @date 2021/6/25
+ */
+@Configuration
+public class SdkAutoConfiguration {
+}
